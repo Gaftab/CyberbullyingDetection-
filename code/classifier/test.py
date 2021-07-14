7315e3b9bb81f3fcb54cbb1d4756dd745276fffa
Gaftab <aftabbaig328@gmail.com>
2021-07-14 09:29:27 +0000

    file modified

--- code/classifier/test.py
+++ code/classifier/test.py
@@ -34,17 +34,19 @@ pandas.options.mode.chained_assignment = None
 csv = r'/content/drive/MyDrive/Project/CyberbullyingDetection-/data/cyberbullying_dataset.csv'
 dataset = pandas.read_csv(csv)
 dataset.head()
+# # dataset["SenderLocation"].unique()
+# print(dataset["SenderLocation"].unique())
 
-# le= preprocessing.LabelEncoder()
-# dataset["SenderLocation"]=le.fit_transform(dataset["SenderLocation"].astype(str))
-# dataset["SenderLocation"].unique()
+le= preprocessing.LabelEncoder()
+dataset["SenderLocation"]=le.fit_transform(dataset["SenderLocation"].astype(str))
+dataset["SenderLocation"].unique()
 # /data= df.drop("SenderLocation", axis='columns')
 # print(dataset.head())
 # print(dataset["SenderLocation"])
 
-feature_cols_sm = ['IsRetweet','IsSelfMentioned','Retweets#','Favorites#','Hashtags#','Medias#','Mentions#','SenderId','SenderAccountYears','SenderFavorites#','SenderFollowings#','SenderFollowers#','SenderStatues#','SenderLocation','Emojis#','Punctuations#','UpperCaseLetter#','Letter#','Symbols#','Words#','TWords#','UWords#','SlangWords#','AvgWordLength']
-# feature_cols_sm = ['Retweets#','Favorites#','Hashtags#','Medias#','Mentions#','SenderLocation','SenderAccountYears','SenderFavorites#','SenderFollowings#','SenderFollowers#','SenderStatues#','IsSelfMentioned']
-# feature_cols_sm = ['Retweets#','Favorites#','SenderLocation','SenderAccountYears','SenderFavorites#','SenderFollowings#','SenderFollowers#','SenderStatues#']
+# feature_cols_sm = ['IsRetweet','IsSelfMentioned','Retweets#','Favorites#','Hashtags#','Medias#','Mentions#','SenderId','SenderAccountYears','SenderFavorites#','SenderFollowings#','SenderFollowers#','SenderStatues#','SenderLocation','Emojis#','Punctuations#','UpperCaseLetter#','Letter#','Symbols#','Words#','TWords#','UWords#','SlangWords#','AvgWordLength']
+# # feature_cols_sm = ['Retweets#','Favorites#','Hashtags#','Medias#','Mentions#','SenderLocation','SenderAccountYears','SenderFavorites#','SenderFollowings#','SenderFollowers#','SenderStatues#','IsSelfMentioned']
+feature_cols_sm = ['Retweets#','Favorites#','SenderLocation','SenderAccountYears','SenderFavorites#','SenderFollowings#','SenderFollowers#','SenderStatues#']
 feature_cols_all=['Text']+feature_cols_sm
 X = dataset[feature_cols_all] # All Features
 
@@ -54,56 +56,56 @@ X = dataset[feature_cols_all] # All Features
 
 # # #The min-max normalization was applied to the numerical social media features of samples in the dataset to remove instability.
 
-# scaler = MinMaxScaler()
-# X[feature_cols_sm] = scaler.fit_transform(X[feature_cols_sm])
+scaler = MinMaxScaler()
+X[feature_cols_sm] = scaler.fit_transform(X[feature_cols_sm])
 
 
 
-# x_text=train=X.Text
+x_text=train=X.Text
 # # x_sm=train=X[feature_cols_sm]
-# x_sm=X[feature_cols_sm]
+x_sm=X[feature_cols_sm]
 
 # #Converting data frame to sparce matrix
-# x_sm=scipy.sparse.csr_matrix(x_sm.values)
-# y = dataset.IsCyberbullying # Target 
+x_sm=scipy.sparse.csr_matrix(x_sm.values)
+y = dataset.IsCyberbullying # Target 
 
 # # # 1.2) Feature Extraction (Textual Features)
 
-# # # The terms' weights were calculated using the Term Frequency - Inverse Document Frequency (TF-IDF)
-# tfidf_vect = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', max_features=50000)
-# tfidf_vect.fit(x_text)
-# x_text_tfidf =  tfidf_vect.transform(x_text)
+# # The terms' weights were calculated using the Term Frequency - Inverse Document Frequency (TF-IDF)
+tfidf_vect = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', max_features=50000)
+tfidf_vect.fit(x_text)
+x_text_tfidf =  tfidf_vect.transform(x_text)
 
 # 1.3) Feature Selection (Textual Features)
 
 # Feature selection  using a chi-square score was applied  for each applied machine learning algorithm to select relevant textual features. 
 
 # COMMENT OUT following code block for experimenting different feature sizes for each classifier
-# clf=LogisticRegression()
-# for x in range(500, 4000, 500):
-#     test = SelectKBest(score_func=chi2, k=x)
-#     fit = test.fit(x_text_tfidf, y)
-#     x_t= fit.transform(x_text_tfidf)
-#     scores = cross_val_score(clf, x_t, y, cv=10)
-#     print(scores)
+clf=LogisticRegression()
+for x in range(500, 4000, 500):
+    test = SelectKBest(score_func=chi2, k=x)
+    fit = test.fit(x_text_tfidf, y)
+    x_t= fit.transform(x_text_tfidf)
+    scores = cross_val_score(clf, x_t, y, cv=10)
+    print(scores)
 
 
-# clf=KNeighborsClassifier()
-# for x in range(500, 4000, 500):
-#     test = SelectKBest(score_func=chi2, k=x)
-#     fit = test.fit(x_text_tfidf, y)
-#     x_t= fit.transform(x_text_tfidf)
-#     scores = cross_val_score(clf, x_t, y, cv=10 )
-#     print(scores)
+clf=KNeighborsClassifier()
+for x in range(500, 4000, 500):
+    test = SelectKBest(score_func=chi2, k=x)
+    fit = test.fit(x_text_tfidf, y)
+    x_t= fit.transform(x_text_tfidf)
+    scores = cross_val_score(clf, x_t, y, cv=10 )
+    print(scores)
 
-# # # Use k that has the most highest scores.
-# test = SelectKBest(score_func=chi2, k=500)
-# fit = test.fit(x_text_tfidf, y)
-# # #x_t only contains social media features
-# x_t= fit.transform(x_text_tfidf)
+# # Use k that has the most highest scores.
+test = SelectKBest(score_func=chi2, k=500)
+fit = test.fit(x_text_tfidf, y)
+# #x_t only contains social media features
+x_t= fit.transform(x_text_tfidf)
 
-# # #x_ts contain social media features in addition to textual features
-# x_ts=hstack((x_t, x_sm))
+# #x_ts contain social media features in addition to textual features
+x_ts=hstack((x_t, x_sm))
 
 
 
@@ -120,7 +122,7 @@ X = dataset[feature_cols_all] # All Features
 
 #Experiment both x; x_ts or x_t
 
-# x=x_ts
+x=x_ts
 
 # search_grid = [{'kernel': ['rbf'], 'gamma': [1e-2, 1e-3, 1e-4, 1e-5],
 #                      'C': [0.001, 0.10, 0.1, 10, 25, 50, 100, 1000]},
@@ -215,13 +217,13 @@ X = dataset[feature_cols_all] # All Features
 # #3.2) KNN
 
 # 3.2.A)  text and social media features
-# clf= KNeighborsClassifier(n_neighbors= 3, metric="euclidean")
-# scores_ts = cross_val_score(clf, x_ts, y, cv=10)
-# knnTs=scores_ts.mean()
-# #3.2.B)  just text features
-# clf=KNeighborsClassifier(n_neighbors= 6, metric="euclidean")
-# scores_t = cross_val_score(clf, x_t, y, cv=10)
-# knnT=scores_t.mean()
+clf= KNeighborsClassifier(n_neighbors= 3, metric="euclidean")
+scores_ts = cross_val_score(clf, x_ts, y, cv=10)
+knnTs=scores_ts.mean()
+#3.2.B)  just text features
+clf=KNeighborsClassifier(n_neighbors= 6, metric="euclidean")
+scores_t = cross_val_score(clf, x_t, y, cv=10)
+knnT=scores_t.mean()
 
 
 # #3.3) NBM
